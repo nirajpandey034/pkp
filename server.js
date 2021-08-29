e74e0705606bcdbaca3d7f0fc15152d12dba2341
Niraj Pandey <nirajpandey034@gmail.com>
2021-08-29 13:37:53 +0530

    added login route

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app =  express();
 app.get('/', function (req, res) {
     res.send('Hello, World!');
 })
+
+app.get('/login', function (req, res) {
+    res.send('login!');
+})
+
+
 app.listen(3000,()=>{
     console.log("listening")
 })
